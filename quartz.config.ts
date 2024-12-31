6cc3389c51fb355cc92592185c544b79de1a18ca
Aleksandrs <aleksandrs.pridoroznijs@outlook.com>
2024-12-31 14:23:53 +0200

    Quartz sync: Dec 31, 2024, 2:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "roadsidemaker.github.io/rsm-blog",
+    baseUrl: "blog.roadsidemaker.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
